321087e0d6e395cb8238244d9d535ef4241e9295
Jacob Millner <jacob.millner@outlook.com>
2022-09-09 18:35:08 -0400

    hook up parser to scraped results

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pprint
 from recipe_scrapers import scrape_me
+from util import parse_ingredient_string
 
 pp = pprint.PrettyPrinter(indent=2)
 
@@ -13,14 +14,19 @@ for recipe in recipes:
     try:
         scraper = scrape_me(recipe)
         ingred = scraper.ingredients()
-        pp.pprint(ingred)
-    except:
-        print('Recipe site not supported, trying wild mode for: ', recipe)
+        for ingred_str in ingred:
+            parsed_ingred = parse_ingredient_string(ingred_str)
+            pp.pprint(parsed_ingred)
+    except Exception as e:
+        print('Scraping error ' + str(e))
+        print('trying wild mode for: ', recipe)
         try:
             scraper = scrape_me(recipe, wild_mode=True)
             ingred = scraper.ingredients()
             if ingred:
-                pp.pprint(ingred)
+                for ingred_str in ingred:
+                    parsed_ingred = parse_ingredient_string(ingred_str)
+                    pp.pprint(parsed_ingred)
             else:
                 print('Nothing found for: ', recipe)
         except:
--- util.py
+++ util.py
@@ -1,5 +1,8 @@
-unit_strings = ['cup', 'tablespoon', 'teaspoon']
-prep_type = ['chopped', 'diced']
+unit_strings = ['cup', 'cups', 'tablespoon', 'tablespoons', 'teaspoon', 'teaspoons',
+        'pound', 'pounds', 'lb', 'lbs']
+prep_type = ['chopped', 'diced', 'minced', 'prepared', 'grated', 
+        'thinly sliced', 'sliced', 'julienned', 'thinly or julienned carrots',
+        'packed']
 
 def parse_ingredient_string(ingredient):
     # create a new dictionary to store parsed ingredient data
@@ -8,29 +11,47 @@ def parse_ingredient_string(ingredient):
     # setup data vars
     measurement = None
     unit = None
+    prep = None
     ingredient_key = None
     
     # split the string in order to loop over each word and classify them
     # example string: "1 cup apple sauce"
     split_ingredients = ingredient.split()
     for text in split_ingredients:
-        if text.isnumeric():
+        if is_measurement(text):
             # is measurement
-            measurement = text
-            split_ingredients.pop(0)
-            pass
+            if measurement is None:
+                measurement = text
+            else:
+                measurement = measurement + ' ' + text
         if text in unit_strings:
             # is unit
             unit = text
-            split_ingredients.pop(0)
-            pass
         if text in prep_type:
-            # ignore and remove prep type
-            split_ingredients.pop(0)
-            pass
+            # save the prep to remove later 
+            prep = text
+    # remove all the gathered words
+    if measurement is not None:
+        meas_list = measurement.split(' ')
+        for mea in meas_list:
+            split_ingredients.remove(mea)
+    if unit is not None:
+        split_ingredients.remove(unit)
+    if prep is not None:
+        split_ingredients.remove(prep)
     # leftover strings in array form the ingredient key
     # i.e. "apple sauce"
     ingredient_key = " ".join(split_ingredients)
     parsed_ingredient[ingredient_key] = {unit: measurement}
     return parsed_ingredient
 
+def is_measurement(text):
+    # isnumeric doesn't account for values like 2/3
+    # so check for those
+    if text.isnumeric():
+        return True
+    elif '/' in text:
+        values = text.split('/')
+        return len(values) == 2 and all(i.isdigit() for i in values)
+    else:
+        return False
